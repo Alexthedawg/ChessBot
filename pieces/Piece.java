4e62fc772db690b8bd6e5925eba475e08d967b95
Alex Wang <iamalex11303@gmail.com>
2023-02-04 19:34:16 -0500

    Hid Piece field

--- pieces/Piece.java
+++ pieces/Piece.java
@@ -1,21 +1,29 @@
 package pieces;
 
+import java.util.AbstractSequentialList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Piece {
 
     /**
-     * The position of the Piece on the Board.
+     * The position of the Piece on the pieces.Board.
      * 0 is the equivalent of A1; 63 is H8.
      */
     private int position;
-
+    /**
+     * The player who will play the next move.
+     */
+    private Alliance alliance;
     /**
      * The "class" of the Piece
      * (i.e. Knight, King, Pawn, etc.).
      */
     private PieceType type;
 
-    public Piece(int position, PieceType type) {
+    public Piece(int position, Alliance alliance, PieceType type) {
         this.position = position;
+        this.alliance = alliance;
         this.type = type;
     }
 
@@ -23,7 +31,9 @@ public class Piece {
 
     public void setPosition(int position) { this.position = position; }
 
-    public PieceType getType() { return type; }
+    public PieceType getType() { return this.type; }
+
+    public Alliance getAlliance() { return this.alliance; }
 
     /**
      * The letter representation of this Piece.
@@ -31,4 +41,29 @@ public class Piece {
      */
     @Override
     public String toString() { return type.name(); }
+
+    /**
+     * Creates a deep copy of this Piece.
+     * @return a deep copy of this Piece.
+     */
+    public Piece clone() {
+        Piece clone = new Piece(this.getPosition(), this.alliance, PieceType.NULL);
+        clone.type = this.getType();
+
+        return clone;
+    }
+
+    /**
+     * Gives a Set of Moves that this Piece can make.
+     * TODO check for invalid moves that leave King in check.
+     * @return a Set of valid Moves.
+     */
+    public Set<Move> getMoves(Board board) {
+        // Gets the square of potential moves.
+        Set<Integer> targets = this.type.getPotentialMoves(board, this.getPosition());
+        // TODO check for invalid moves due to exposed King.
+        return targets.stream()
+                      .map(target -> new Move(this.getPosition(), target))
+                      .collect(Collectors.toSet());
+    }
 }
