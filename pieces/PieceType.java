f98e95851170f1af6a1e50ef78f0b697966b990e
Alex Wang <iamalex11303@gmail.com>
2023-02-04 19:43:35 -0500

    Integrated this enum to work with the MoveUtils class to calculate moves based on the PieceType of a Piece

--- pieces/PieceType.java
+++ pieces/PieceType.java
@@ -1,23 +1,34 @@
 package pieces;
 
+import javax.print.attribute.SetOfIntegerSyntax;
+import java.util.Set;
+import java.util.function.BiFunction;
+
 /**
  * Represents the various Pieces on the chessboard.
  * Stores the letter representations of each type
  * (e.g. B for Bishop, N for Knight, P for Pawn).
  */
 public enum PieceType {
-    KING("K"),
-    QUEEN("Q"),
-    BISHOP("B"),
-    KNIGHT("N"),
-    ROOK("R"),
-    PAWN("P"),
-    NULL("-");
+    //TODO finish these move functions.
+    KING("K", null),
+    QUEEN("Q", null),
+    BISHOP("B", null),
+    KNIGHT("N", null),
+    ROOK("R", MoveUtils.ROOK_MOVES),
+    PAWN("P", null),
+    NULL("-", null);
 
     private final String name;
+    private final BiFunction<Board, Integer, Set<Integer>> moves;
 
-    PieceType(String name) {
+    PieceType(String name, BiFunction<Board, Integer, Set<Integer>> moves) {
         this.name = name;
+        this.moves = moves;
+    }
+
+    public final Set<Integer> getPotentialMoves(Board board, final int position) {
+        return this.moves.apply(board, position);
     }
 
     @Override
