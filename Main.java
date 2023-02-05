e59da45b2d4d008f58f256647dc9c14f0ef683fe
Alex Wang <iamalex11303@gmail.com>
2023-02-04 19:49:15 -0500

    Checkpoint 2

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-import pieces.Piece;
+import pieces.Alliance;
+import pieces.Board;
+import pieces.Move;
 import pieces.PieceType;
+
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
-        System.out.println(board);
+        Board board = new Board(Alliance.WHITE);
+
+        Board copy = new Board(board);
+        Set<Move> moves = board.pieces.get(0).getMoves(board);
+
+        System.out.println(moves);
     }
 }
